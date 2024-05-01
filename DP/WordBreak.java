f129ef1a3adc4590c3c2e3fc033891aa715ccf06
Priya Gupta <37833111+Priygupt13@users.noreply.github.com>
2024-05-01 16:39:38 -0700

    Update and rename WordBreak to WordBreak.java

---
+++ DP/WordBreak.java
@@ -0,0 +1,44 @@
+/**
+ * Date 05/01/2024
+ * @author Priya
+ * 
+ * Given a string and a dictionary, split this string into multiple words such that
+ * each word belongs in dictionary.
+ * 
+ * e.g peanutbutter -> pea nut butter
+ * e.g Iliketoplay -> I like to play
+ * 
+ * Solution 
+ * DP solution to this problem
+ * if( input[i...j] belongs in dictionary) T[i][j] = i
+ * else{
+ *     T[i][j] = k if T[i][k-1] != -1 && T[k][j] != -1
+ *     
+ * Test cases
+ * 1) String where entire string is in dictionary
+ * 2) String which cannot be split into words which are in dictionary
+ * 3) String which can be split into words which are in dictionary    
+ */
+
+class Solution {           
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()+1];
+        dp[dp.length-1] = true;
+        for(int i=s.length()-1;i>=0;--i){ // "catsandog"
+            for(int j=0;j<wordDict.size();++j){
+               String word = wordDict.get(j);  // cats
+               int len = i + word.length();
+               if(len>s.length()) continue;
+                 if(s.startsWith(word,i)){
+                    dp[i] = dp[i+word.length()];
+                 }
+                 if(dp[i]) break;
+            }
+        }
+        return dp[0];
+    }
+}
+// TC O(n*m*n)
+// SC O(n)
+
+
